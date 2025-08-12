ea1c9c595e93cf812e5a7fd064aeba68b6af0b26
sergiocalazans <calazanssergiohenrique@gmail.com>
2025-08-12 10:27:36 -0300

    Update tipo_double.c

--- aula-2/double/tipo_double.c
+++ aula-2/double/tipo_double.c
@@ -5,7 +5,15 @@ int main()
     double PI = 3.14;
     double raio = 10.0;
     double area = PI * raio * raio;
-    printf("%f\n", area);
+    printf("Area: %f\n", area);
+
+    // sizeof para imprimir o tamanho em bytes
+    /*
+    %zu é um especificador de formato usado com a função printf para imprimir valores do tipo size_t. 
+    size_t é um tipo inteiro sem sinal definido pelo sistema, 
+    normalmente usado para representar tamanhos de objetos ou índices em matrizes. 
+    */
     printf("%zu\n", sizeof(double));
+    printf("%zu\n", sizeof(PI));
     return 0;
 }

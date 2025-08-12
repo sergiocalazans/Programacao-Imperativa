730cb97534358c02b1737c4079fd0ae3599743d2
sergiocalazans <calazanssergiohenrique@gmail.com>
2025-08-12 11:18:15 -0300

    Update tipo_double.c and tipo_int.c

--- aula-2/double/tipo_double.c
+++ aula-2/double/tipo_double.c
@@ -7,13 +7,21 @@ int main()
     double area = PI * raio * raio;
     printf("Area: %f\n", area);
 
+    printf("\nLocalidade da memoria\n");
+    printf("%p\n", &area);
+    printf("%p\n", &raio);
+    printf("%p\n", &PI);
+
     // sizeof para imprimir o tamanho em bytes
     /*
     %zu é um especificador de formato usado com a função printf para imprimir valores do tipo size_t. 
     size_t é um tipo inteiro sem sinal definido pelo sistema, 
     normalmente usado para representar tamanhos de objetos ou índices em matrizes. 
     */
-    printf("%zu\n", sizeof(double));
-    printf("%zu\n", sizeof(PI));
+    printf("\nVerificar o tamanho em bytes\n");
+    printf("Tipo double: %zu\n", sizeof(double));
+    printf("PI: %zu\n", sizeof(PI));
+    printf("Raio: %zu\n", sizeof(raio));
+    printf("Area: %zu\n", sizeof(area));
     return 0;
 }
--- aula-2/int/tipo_int.c
+++ aula-2/int/tipo_int.c
@@ -4,10 +4,21 @@ int main()
 {
     int temperatura = 20;
     temperatura = -10;
+
+    // elimina o bit de sinal, dobrando o intervalo de implementação
     unsigned int idade = 30;
     idade = 40;
-    printf("%d %d\n", temperatura,
-           idade);
+
+    printf("\nTemperatura e idade em decimal:\n");
+    printf("%d %d\n", temperatura, idade);
+
+    printf("\nVerificar o tamanho em bytes\n");
     printf("%zu\n", sizeof(int));
+    printf("%zu\n", sizeof(temperatura));
+    printf("%zu\n", sizeof(idade));
+
+    printf("\nTemperatura em hexa: %x\n", temperatura);
+    printf("Idade em hexa: %x\n", idade);
+
     return 0;
 }

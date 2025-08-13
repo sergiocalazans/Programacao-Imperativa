f46a014f49d3d8850e32025dd938d185160d0d00
sergiocalazans <calazanssergiohenrique@gmail.com>
2025-08-13 07:41:46 -0300

    Create ex_int.c

---
+++ aula-2/exercicios/ex_int.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+
+    /*
+    Faça a representação simbólica da memória do
+    sistema para as variáveis definidas no programa
+    contido no arquivo tipo_int.c
+    */
+
+    int temperatura = 20;
+    temperatura = -10;
+
+    // elimina o bit de sinal, dobrando o intervalo de implementação
+    unsigned int idade = 30;
+    idade = 40;
+
+    printf("\nTemperatura e idade em decimal:\n");
+    printf("%d %d\n", temperatura, idade);
+
+    printf("\nVerificar o tamanho em bytes\n");
+    printf("%zu\n", sizeof(int));
+    printf("%zu\n", sizeof(temperatura));
+    printf("%zu\n", sizeof(idade));
+
+    printf("\nTemperatura em hexa: %x\n", temperatura);
+    printf("Idade em hexa: %x\n", idade);
+
+    printf("\nLocalidade na memoria\n");
+    printf("Temperatura: %#X\n", &temperatura);
+    printf("Idade: %#X\n", &idade);
+
+    return 0;
+}
